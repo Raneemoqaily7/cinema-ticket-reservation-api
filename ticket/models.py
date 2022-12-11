d9f6f74a4d9f4f9219649b6d5d046a18e0a99b54
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-12-11 15:56:25 +0000

    models done

---
+++ ticket/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie (models.Model):
+    titel = models.CharField(max_length= 30)
+    hall = models.CharField(max_length=25)
+    date=models.DateField()
+
+
+    def __str__(self):
+        return self.titel
+
+class Guest (models.Model):
+    name = models.CharField (max_length= 30)
+    number =models.CharField (max_length= 10)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation (models.Model):
+    guest =models.ForeignKey(Guest , related_name="reservation" , on_delete=models.CASCADE )
+    movie = models.ForeignKey(Movie ,  on_delete=models.CASCADE )
+
+
+
+
+    def __str__(self):
+        return str (self.movie)
